950ba241b4153cb911bf12e039ab9f1ac9e98a55
Supriya H P <supriyapriya33s@users.noreply.github.com>
2020-07-15 23:35:10 +0530

    Updates

---
+++ precache-manifest.1a82cdc32772d216200ccbc33f2410dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011110dcc175e9780be5ed27ba3329d5",
+    "url": "/react-typewriter/index.html"
+  },
+  {
+    "revision": "fff1e4fffee1138e7a10",
+    "url": "/react-typewriter/static/js/0.2ec16310.chunk.js"
+  },
+  {
+    "revision": "50d43d2d22200adf7fb6",
+    "url": "/react-typewriter/static/js/5.76d5384f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-typewriter/static/js/5.76d5384f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ce97b97da2d6020cea",
+    "url": "/react-typewriter/static/js/6.033f77e4.chunk.js"
+  },
+  {
+    "revision": "d0411ef8a131d741d2cb",
+    "url": "/react-typewriter/static/js/Results.a8d8c88a.chunk.js"
+  },
+  {
+    "revision": "fedc2c1cae9a2dcbcdde",
+    "url": "/react-typewriter/static/js/TypeWriter.106a653f.chunk.js"
+  },
+  {
+    "revision": "180445f625ac061fb58c",
+    "url": "/react-typewriter/static/js/main.ad6960ef.chunk.js"
+  },
+  {
+    "revision": "a3ec754c31058c078573",
+    "url": "/react-typewriter/static/js/runtime-main.874d712c.js"
+  }
+]);
